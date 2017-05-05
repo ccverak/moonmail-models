e2dae1c12672f6150b926b041ce28ceaa4db3f38
David García <david.garcia@microapps.com>
2017-05-05 11:06:14 +0200

    Automations and ScheduledEmail models

---
+++ src/models/scheduled_email.js
@@ -0,0 +1,36 @@
+import moment from 'moment';
+import { debug } from './../logger';
+import { Model } from './model';
+
+class ScheduledEmail extends Model {
+  static get tableName() {
+    return process.env.SCHEDULED_EMAILS_TABLE;
+  }
+
+  static get hashKey() {
+    return 'id';
+  }
+
+  static get rangeKey() {
+    return 'scheduledAt';
+  }
+
+  static toBeSent() {
+    debug('= ScheduledEmail.toBeSent');
+    const params = {
+      TableName: this.tableName,
+      FilterExpression: 'scheduledAt < :now and #status = :status and attribute_not_exists(sentAt)',
+      ExpressionAttributeValues: {
+        ':now': moment().unix(),
+        ':status': 'scheduled'
+      },
+      ExpressionAttributeNames: {
+        '#status': 'status'
+      }
+    };
+    return this._client('scan', params)
+      .then(result => result.Items);
+  }
+}
+
+module.exports.ScheduledEmail = ScheduledEmail;
