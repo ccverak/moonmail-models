b64b6b2815905f032f126adec43d69480584783e
Carlos Castellanos Vera <ccverak@gmail.com>
2017-05-30 15:31:35 +0200

    Adding segments model

---
+++ src/models/list_segment.js
@@ -0,0 +1,42 @@
+import Joi from 'joi';
+import { Model } from './model';
+
+class ListSegment extends Model {
+
+  static get tableName() {
+    return process.env.LIST_SEGMENTS_TABLE;
+  }
+
+  static get hashKey() {
+    return 'listId';
+  }
+
+  static get rangeKey() {
+    return 'id';
+  }
+
+  static get schema() {
+    return Joi.object({
+      listId: Joi.string().required(),
+      id: Joi.string().required(),
+      userId: Joi.string().required(),
+      name: Joi.string().required(),
+      conditionMatch: Joi.string().required(),
+      conditions: Joi.array().items(Joi.object().keys({
+        conditionType: Joi.string().required(),
+        condition: Joi.object().keys({
+          op: Joi.string().required(),
+          field: Joi.string().required(),
+          value: Joi.any().required()
+        })
+      })).min(1)
+    });
+  }
+
+  static save(item) {
+    if (this.isValid(item)) return super.save(item);
+    return Promise.reject(new Error('ListSegment is not valid'));
+  }
+}
+
+module.exports.ListSegment = ListSegment;
---
+++ tests/list_segment.test.js
@@ -0,0 +1,65 @@
+import * as chai from 'chai';
+const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+import * as sinon from 'sinon';
+import * as sinonAsPromised from 'sinon-as-promised';
+import { ListSegment } from '../src/models/list_segment';
+
+chai.use(chaiAsPromised);
+
+describe('ListSegment', () => {
+  const tableName = 'list_segments-table';
+  const hashKey = 'listId';
+  const rangeKey = 'id';
+  let tNameStub;
+
+  before(() => {
+    sinon.stub(ListSegment, '_client').resolves(true);
+    tNameStub = sinon.stub(ListSegment, 'tableName', { get: () => tableName });
+  });
+
+  describe('#hashKey', () => {
+    it('returns the hash key name', () => {
+      expect(ListSegment.hashKey).to.equal(hashKey);
+    });
+  });
+
+  describe('#rangeKey', () => {
+    it('returns the range key name', () => {
+      expect(ListSegment.rangeKey).to.equal(rangeKey);
+    });
+  });
+
+  describe('#save', () => {
+    context('when the item is not valid', () => {
+      it('rejects and returns', (done) => {
+        ListSegment.save({ listId: '1', id: '2' }).catch(() => {
+          expect(ListSegment._client).not.to.have.been.called;
+          done();
+        });
+      });
+    });
+    context('when the item is valid', () => {
+      it('saves it', (done) => {
+        ListSegment.save({
+          listId: '1',
+          id: '2',
+          name: 'some-name',
+          userId: 'user-id',
+          conditionMatch: 'any',
+          conditions: [
+            { conditionType: 'subscription-origin', condition: { op: 'eq', field: 'country', value: 'ES' } }
+          ]
+        }).then(() => {
+          expect(ListSegment._client).to.have.been.called;
+          done();
+        });
+      });
+    });
+  });
+
+  after(() => {
+    ListSegment._client.restore();
+    tNameStub.restore();
+  });
+});
